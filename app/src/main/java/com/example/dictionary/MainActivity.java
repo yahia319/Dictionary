16313c12f22ece6a1e5c1b06cd74c03033c661fa
yahia319 <yahia.hallas19@gmail.com>
2020-12-15 18:10:47 +0100

    commit 1

--- app/src/main/java/com/example/dictionary/MainActivity.java
+++ app/src/main/java/com/example/dictionary/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.dictionary;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.SearchView;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,8 +20,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     TextView textViewLanguage1, textViewLanguage2, textViewReverse;
-
-
+    SearchView searchView;
+    myAdapter myAdapter;
+    ArrayList<list_Item> Items;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,16 +55,18 @@ public class MainActivity extends AppCompatActivity {
             String mLine;
 
 
-            ArrayList<list_Item> Items = new ArrayList<list_Item>();
+            Items = new ArrayList<list_Item>();
 
             while ((mLine = reader.readLine()) != null) {
-                String[]   elements = mLine.split("#");
+                String[] elements = mLine.split("#");
                 Items.add(new list_Item(elements[0], elements[1]));
             }//end while
-            myAdapter myAdapter = new myAdapter(Items);
 
-            ListView listView = findViewById(R.id.list_view);
+            myAdapter = new myAdapter(getApplicationContext(),Items);
+
+            listView = findViewById(R.id.list_view);
             listView.setAdapter(myAdapter);
+
         }//end try
 
         catch (IOException e) {
@@ -68,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
         }//end catch
 
         finally {
+            //
             if (reader != null) {
                 try {
                     reader.close();
@@ -80,13 +87,73 @@ public class MainActivity extends AppCompatActivity {
 
         }//end finally
 
+            searchView = findViewById(R.id.search_view);
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    // TODO: filter list using query
+              myAdapter.getFilter().filter(query);
+                    // update adapter using list then refresh it.
+                    return false;
+                }
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            });
+
+
+
     }// end onCreate
 
-    class myAdapter extends BaseAdapter {
+    public class myAdapter extends ArrayAdapter<list_Item> {
+
         ArrayList<list_Item> Items = new ArrayList<list_Item>();
 
-        myAdapter(ArrayList<list_Item> Items) {
+        public myAdapter( Context context,ArrayList<list_Item> Items) {
+            super(context, 0);
             this.Items = Items;
+        }//end const
+
+        @NonNull
+        @Override
+        public Filter getFilter() {
+            return new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+
+                    ArrayList<list_Item> elements = new ArrayList();
+
+                    if(constraint == null || constraint.length() == 0 ){
+                        elements.addAll(Items);
+                    }else{
+                        String filterParent = constraint.toString().toLowerCase();
+                        for(list_Item element : Items){
+                            if ((element.getNameEng().toLowerCase().contains(filterParent))){
+                                elements.add(element);
+                            }else{
+                                if (element.getNameAr().toLowerCase().contains(filterParent)){
+                                    elements.add(element);
+                                }
+                            }
+                        }
+                    }//end else
+
+                    FilterResults results = new FilterResults();
+
+                    results.count = elements.size();
+                    results.values = elements;
+
+                    return results;
+                }
+
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results) {
+                    clear();
+                    addAll((ArrayList<list_Item>) results.values);
+                    notifyDataSetChanged();
+                }
+            };
         }
 
         @Override
@@ -95,8 +162,8 @@ public class MainActivity extends AppCompatActivity {
         }//end getCount
 
         @Override
-        public String getItem(int position) {
-            return Items.get(position).nameEng;
+        public list_Item getItem(int position) {
+            return Items.get(position);
         }//end getItem
 
 
@@ -110,13 +177,16 @@ public class MainActivity extends AppCompatActivity {
             LayoutInflater lInflater = getLayoutInflater();
             View view1 = lInflater.inflate(R.layout.row_view, null);
 
-            TextView textName = view1.findViewById(R.id.name_eng);
-            textName.setText(Items.get(i).nameEng);
+            TextView nameEng = view1.findViewById(R.id.name_eng);
+            nameEng.setText(Items.get(i).getNameEng());
 
-            TextView textJob = view1.findViewById(R.id.name_ar);
-            textJob.setText(Items.get(i).nameAr);
+            TextView nameAr = view1.findViewById(R.id.name_ar);
+            nameAr.setText(Items.get(i).getNameAr());
 
             return view1;
         }//end getView
+
+
     }//end myAdapter
-}
+
+}//end class
--- app/src/main/java/com/example/dictionary/list_Item.java
+++ app/src/main/java/com/example/dictionary/list_Item.java
@@ -1,11 +1,29 @@
 package com.example.dictionary;
 
 public class list_Item {
-    public String nameEng;
-    public String nameAr ;
+
+    private String nameEng;
+    private String nameAr ;
 
     public list_Item(String nameEng,String nameAr){
         this.nameEng = nameEng;
         this.nameAr = nameAr ;
+    }
+
+    public String getNameEng() {
+        return nameEng;
+    }
+
+    public void setNameEng(String nameEng) {
+        this.nameEng = nameEng;
+    }
+
+
+    public String getNameAr() {
+        return nameAr;
+    }
+
+    public void setNameAr(String nameAr) {
+        this.nameAr = nameAr;
     }
 }
