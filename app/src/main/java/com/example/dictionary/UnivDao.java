aceb884d059864f78129aa00575ef75156184ab0
yahia319 <yahia.hallas19@gmail.com>
2020-12-25 14:30:04 +0100

    Revert "test database"
    This reverts commit 1cae6468dd1e4819c63af5ec038ece315fb64e11.
    
    # Conflicts:
    #	app/src/main/java/com/example/dictionary/MainActivity.java
    #	app/src/main/java/com/example/dictionary/UnivRoomDb.java

--- app/src/main/java/com/example/dictionary/UnivDao.java
+++ app/src/main/java/com/example/dictionary/UnivDao.java
@@ -11,9 +11,6 @@ public interface UnivDao {
     @Query("SELECT * FROM univ WHERE ARABIC_NORMALIZED LIKE:arabic")
     List<Univ> getArabicTranslate(String arabic);
 
-    @Query("SELECT * FROM univ")
-    List<Univ> getAll();
-
 
     @Query("SELECT *  FROM univ WHERE FRENCH_NORMALIZED LIKE:french")
     List<Univ> getFrenchTranslate(String french);
